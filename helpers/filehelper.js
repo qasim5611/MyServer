5f87727c977d9a961296ae94e3a57dcc67d167f5
qasim5611 <qmuhammad144@gmail.com>
2022-06-13 17:13:40 +0500

    was

--- helpers/filehelper.js
+++ helpers/filehelper.js
@@ -1,23 +1,49 @@
+const express = require("express");
+
+const app = express();
+const path = require("path");
+const cors = require("cors");
 // 'use strict';
-const multer = require('multer');
+const multer = require("multer");
+
+app.use(cors());
+
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './../uploads');
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
-    }
+  destination: (req, file, cb) => {
+    cb(null, "./../uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
 });
 const filefilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' 
-        || file.mimetype === 'image/jpeg'){
-            cb(null, true);
-        }else {
-            cb(null, false);
-        }
-}
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  next();
+});
 
 const uploadall = multer({ storage: storage, fileFilter: filefilter });
 

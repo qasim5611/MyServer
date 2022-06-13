875be298a88fa8621d06159e7bbf722ed94ba33d
qasim5611 <qmuhammad144@gmail.com>
2022-06-13 19:19:17 +0500

    wsd

--- index.js
+++ index.js
@@ -20,28 +20,17 @@ app.use(cors({ origin: "http://localhost:8080" }));
 app.use(cors({ origin: "https://ap.ragdollcatnft.com" }));
 
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
+// // Add headers before the routes are defined
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-    res.setHeader("Access-Control-Allow-Origin", "https://ap.ragdollcatnft.com");
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+//     res.setHeader("Access-Control-Allow-Origin", "https://ap.ragdollcatnft.com");
 
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 
 var bodyParser = require("body-parser");

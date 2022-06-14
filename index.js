3671d924d784e7fea0e7a9b57d0549d79bbe8f45
qasim5611 <qmuhammad144@gmail.com>
2022-06-14 14:29:36 +0500

    qasl

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const app = express();
 const path = require("path");
 
 
-const cors = require("cors");
-const corsOptions = {
-  origin: "http://server.ragdollcatnft.com",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const cors = require("cors");
 
+// app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 
 var bodyParser = require("body-parser");

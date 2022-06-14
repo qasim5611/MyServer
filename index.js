b42fdfa4133d7700aad3dc805b8490d3ed02f010
qasim5611 <qmuhammad144@gmail.com>
2022-06-14 14:43:31 +0500

    res

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const path = require("path");
 
 // app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
 
 
 var bodyParser = require("body-parser");

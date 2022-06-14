d4e9fd58fc46e52dd3f64a650c782824531db4f7
qasim5611 <qmuhammad144@gmail.com>
2022-06-14 18:21:42 +0500

    qasfinal

--- helpers/filehelper.js
+++ helpers/filehelper.js
@@ -11,7 +11,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./../uploads");
+    cb(null, "./uploads");
   },
   filename: (req, file, cb) => {
     cb(
